f3167d3a95f6fb09bddd7a4828385111f8af6719
Sushant Sharma <sushant.x01@gmail.com>
2022-08-27 15:49:14 +0530

    User Schema created

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = 5000;
 dotenv.config();
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pwuas.mongodb.net/users?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pwuas.mongodb.net/UsersDB?retryWrites=true&w=majority`,
   () => {
     console.log("Connected to the database.");
   }
