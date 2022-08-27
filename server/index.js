44e928ec32293aaa34b614a8db32a82c1409e7aa
Sushant Sharma <sushant.x01@gmail.com>
2022-08-27 15:26:39 +0530

    Connected to the database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const app = express();
 const PORT = 5000;
+dotenv.config();
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.pwuas.mongodb.net/users?retryWrites=true&w=majority`,
+  () => {
+    console.log("Connected to the database.");
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("hello world");
